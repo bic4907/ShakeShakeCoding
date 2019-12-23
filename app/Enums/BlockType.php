222c11c2e04a15f5261ea95809bf7c7077986e1f
eogh234 <eogh234@naver.com>
2019-12-23 20:57:30 +0900

    enum / model / migration 생성

---
+++ app/Enums/BlockType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class BlockType extends Enum
+{
+    const Block =   0;
+    const Input =   1;
+}
