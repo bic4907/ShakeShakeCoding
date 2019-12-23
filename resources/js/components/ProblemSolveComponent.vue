023d30f4b0916871a73c4750a0a0cc742ff8cb7f
bic4907 <bic4907@gmail.com>
2019-12-23 22:22:22 +0900

    Vue 적용완료

---
+++ resources/js/components/ProblemSolveComponent.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "problem-solve-component",
+        data: function() {
+            return {
+                message: 'hr'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,7 +13,6 @@
     <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-
     {{ Html::style('css/style.css') }}
 
     {{ Html::style('css/amaran.min.css') }}
@@ -28,35 +27,24 @@
     </title>
 </head>
 <body>
+    <div id="app">
+
+
+        <section>
+            @yield('content')
+        </section>
+
+        <footer class="page-footer font-small pt-5 mt-5">
+
+        </footer>
 
 
 
-<section>
-    @yield('content')
-</section>
-
-<footer class="page-footer font-small pt-5 mt-5">
-
-</footer>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
 
-    <!--<script src="http://{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>-->
 
-
-    {{ Html::script('js/app.js') }}
-    <!--{{ Html::script('js/loading.js') }}-->
-    {{ Html::script('js/jquery.amaran.js') }}
-
-    <script>
-        $(function () {
-            $('[data-toggle="tooltip"]').tooltip()
-        })
-
-    </script>
-
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
