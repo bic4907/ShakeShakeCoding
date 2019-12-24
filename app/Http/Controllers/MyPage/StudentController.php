34462a18e0116278a1a8a7e0a03751ad925323bd
jaewo <choejwoo05@naver.com>
2019-12-24 14:26:53 +0900

    mypage pagination

--- app/Http/Controllers/MyPage/StudentController.php
+++ app/Http/Controllers/MyPage/StudentController.php
@@ -7,7 +7,10 @@ use App\Question;
 use App\Submission;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Collection;
 
 class StudentController extends Controller
 {
@@ -18,20 +21,31 @@ class StudentController extends Controller
 
         $user = Auth::user();
         //->where('reg_state','=', RegisterType::Registered)나중에 추가
-        $submissions = Submission::where('student_id','=',$user->id)->orderBy('id','desc')->paginate(10);
+        $submissions = Submission::where('student_id','=',$user->id)->get();
         $submissionListData = array();
 
         foreach($submissions as $submission) {
             $question = Question::where('id','=',$submission->question_id)->first();
             $professor = User::where('id', '=', $question->professor_id)->first();
 
-            $submissionListData[] = array(
+            $submissionArray[] = array(
                 'questionId' => $question->id,
                 'professorName' => $professor->name,
                 'isCorrect' => $submission->isCorrect
             );
+            $submissionListData = collect($submissionArray);
         }
-        dd($submissionListData);
+
+        $submissionListData  = $this->paginate($submissionListData);
+
         return view('auth.student', ['submissionListData' => $submissionListData]);
     }
+
+    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+    }
+
 }
