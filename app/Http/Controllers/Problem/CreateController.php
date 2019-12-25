2ace442eaf7a6a793d0d03d0e9cd9e6b6dbd42f3
minchan <ahwere@naver.com>
2019-12-25 23:56:58 +0900

    block, line save

--- app/Http/Controllers/Problem/CreateController.php
+++ app/Http/Controllers/Problem/CreateController.php
@@ -2,29 +2,51 @@
 
 namespace App\Http\Controllers\Problem;
 
+use App\Block;
 use App\Enums\BlockType;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
 
 class CreateController extends Controller
 {
     function showAnswer($problem_num){
-        return view('problem/create', ['problem_num'=>$problem_num]);
+        return view('question/create', ['problem_num'=>$problem_num]);
     }
 
     function addBlinkBlock($problem_num, Request $request){
-        if(!$request->blink){   //블
-            dd($problem_num);
+
+        $temp = explode('!!]]', $request->text);
+        for($i=0;$i<sizeof($temp);$i++){
+            $result[$i] = strstr($temp[$i], '[[!!');
+            $result[$i] = substr($result[$i], 4);
+
             $todo_block = new Block();
             $todo_block->question_id = $problem_num;
-            $todo_block->enum = BlockType::Block;
-            $todo_block->text = $request->block;
+            $todo_block->type = '0';
+            $todo_block->content = $result[$i];
+            $todo_block->save();
         }
-        else{
-//            $todo->question_id = ;
-//            $todo->enum = Blocktype::Input;
-//            $todo->text = $request->block;
 
+        $block = $request->text;
+
+        for($i=0;$i<sizeof($temp)-1;$i++){
+            $result[$i] = '[[!!'.$result[$i].'!!]]';
+            $temp_block = explode($result[$i], $block);
+            $block = $temp_block[0].$temp_block[1];
+        }
+
+        $block = explode('<br>', $block);
+
+        echo(strlen($block[5]));
+        echo($block[5]);
+
+        for($i=0;$i<sizeof($block);$i++){
+            $todo_block = new Block();
+            $todo_block->question_id = $problem_num;
+            $todo_block->type = '1';
+            $todo_block->content = $block[$i];
+            $todo_block->save();
         }
     }
 }
--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -1,43 +1,53 @@
 @extends('layouts.default')
 
-
 @section('content')
-
     <div>
-        for i in range(10) :</br>
-        sum += i</br>
-        </br>
-        print('sum = %d', sum)</br>
-        </br>
-
-        <button type="submit" onclick="blink_button()">블링크</button>
-        <button type="submit" onclick="block_button()">블럭</button>
+        <div id="getEverything">
+            for i in range(10) :</br>
+            sum += i</br>
+            </br>
+            print('sum = %d', sum)</br>
+            </br>
+        </div>
+        <button type="submit" onclick="wfSel.replace('[[input:'+randomString()+']]')">블링크</button>
+        <button type="submit" onclick="wfSel.replace('[[!!'+wfSel.getTEXT()+'!!]]')">블럭</button>
+        <button type="submit" onclick="something()">제출</button>
+        {{--        <button type="submit" onclick="blink_button()">블링크</button>--}}
+        {{--        <button type="submit" onclick="block_button()">블럭</button>--}}
 
         <script type="text/javascript">
-            function selectText() {
-                var selectionText = "";
-                if (document.getSelection) {
-                    selectionText = document.getSelection();
-                } else if (document.selection) {
-                    selectionText = document.selection.createRange().text;
-                }
 
-                return String(selectionText);
-            }
-
-            function blink_button() {
-                // alert(selectText());
+            function something() {
                 var param = {
-                    blink: selectText()
+                    text: $("#getEverything").html()
                 }
                 console.log(param)
                 $.ajax({
                     headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
                     type: "POST",
-                    url: "/create/",
+                    url: "/create/{{$problem_num}}",
                     data: param,
                     success: function (data) {
-                        console.log(data);
+                    },
+                    error: function (xhr, status, error) {
+                        alert(error);
+                    }
+                });
+            }
+
+            /*
+            function blink_button() {
+                var param = {
+                    blink: wfSel.getTEXT()
+                }
+                console.log(param)
+                $.ajax({
+                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                    type: "POST",
+                    url: "/create/{{$problem_num}}",
+                    data: param,
+                    success: function (data) {
+                        wfSel.replace('[[input:'+randomString()+']]');
                     },
                     error: function (xhr, status, error) {
                         alert(error);
@@ -46,27 +56,73 @@
             }
 
             function block_button() {
-                // alert(selectText());
                 var param = {
-                    block: selectText()
+                    block: wfSel.getTEXT()
                 }
                 console.log(param)
                 $.ajax({
                     headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
                     type: "POST",
-                    url: "/create",
+                    url: "/create/{{$problem_num}}",
                     data: param,
                     success: function (data) {
-                        console.log(data);
+                        wfSel.replace('{' + param.block + '}');
                     },
                     error: function (xhr, status, error) {
                         alert(error);
                     }
                 });
             }
+*/
+            wfSel = {};
+            (function (_self) {
+                var _sel = window.getSelection();
+                if (_sel) {
+                    _self = {
+                        getTEXT: function () {
+                            var _range = window.getSelection().getRangeAt(0);
+                            return _sel.toString();
+                        },
+                        getHTML: function () {
+                            var _range = window.getSelection().getRangeAt(0),
+                                _content = _range.cloneContents(),
+                                _span = document.createElement('span');
+                            _span.appendChild(_content);
+                            return _span.innerHTML;
+                        },
+                        insert(_before, _after) {
+                            _before = _before ? _before : '';
+                            _after = _after ? _after : '';
+                            this.replace(_before + wfSel.getHTML() + _after);
+                        },
+                        replace: function (text) {
+                            var _range = window.getSelection().getRangeAt(0);
+                            var _node = document.createElement('span');
+                            _node.innerHTML = text;
+                            if (_node) _node = _node.childNodes[0];
+                            _range.deleteContents();
+                            _range.insertNode(_node);
+                        },
+                        removeTag: function () {
+                            this.replace(wfSel.getTEXT());
+                        }
+                    };
+                    window.wfSel = _self;
+                }
+            })(wfSel);
+
+            function randomString() {
+                var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+                var string_length = 5;
+                var randomstring = '';
+                for (var i = 0; i < string_length; i++) {
+                    var rnum = Math.floor(Math.random() * chars.length);
+                    randomstring += chars.substring(rnum, rnum + 1);
+                }
+                return randomstring;
+            }
+
         </script>
-
     </div>
-
 
 @endsection
