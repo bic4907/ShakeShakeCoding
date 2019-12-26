3f493b0e0d4dd0035685c02f1d11763ffed13fda
eogh234 <eogh234@naver.com>
2019-12-26 14:02:34 +0900

    question list 뷰에 question add 버튼 추가

--- resources/views/question/list.blade.php
+++ resources/views/question/list.blade.php
@@ -12,6 +12,12 @@
 
     <div class = "container">
         <div class="list-group py-5">
+            @if(Auth::check() && Auth::user()->usertype == App\Enums\UserType::Professor)
+            <div class="mt-5">
+                <a href="{{route('question.add')}}"><button type="button" class="btn btn-outline-secondary float-right">문제 출제하기</button></a>
+            </div>
+            </br>
+            @endif
             @foreach($questions as $question)
                 <a href="{{route('question.view', ['question_id' => $question->id])}}" class="list-group-item list-group-item-action flex-column align-items-start">
                     <div class="d-flex w-100 justify-content-between">
