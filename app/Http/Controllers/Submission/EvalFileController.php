b18e8d470c9610c899396502e892ecf67ae13a81
jaewo <choejwoo05@naver.com>
2019-12-27 04:21:18 +0900

    final version

--- app/Http/Controllers/Submission/EvalFileController.php
+++ app/Http/Controllers/Submission/EvalFileController.php
@@ -9,6 +9,7 @@ use App\TestCase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use mysql_xdevapi\Exception;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
@@ -16,14 +17,14 @@ use Symfony\Component\Process\Process;
 
 class EvalFileController extends Controller
 {
-    public function grade($question_num, $submission_num , Request $request) {
+    public function grade($question_num, $submission_num, Request $request)
+    {
 
         /**
          * 제출된 파일 모델 생성
          */
-        $submissionFile = SubmissionFile::where('submission_id',$submission_num)->first();
-        if($submissionFile == null)
-        {
+        $submissionFile = SubmissionFile::where('submission_id', $submission_num)->first();
+        if ($submissionFile == null) {
             $submissionFile = new SubmissionFile();
             $user = Auth::user();
             $submissionFile->uuid = Uuid::uuid4();
@@ -38,7 +39,7 @@ class EvalFileController extends Controller
         $filepath = '/home/vagrant/code/storage/app/file/'; // 파일 경로 생성
         FileTransformController::fileTransform(
             $submissionFile,
-            $filepath ,
+            $filepath,
             $request->input('blocks')
         );
 
@@ -54,18 +55,17 @@ class EvalFileController extends Controller
 
     public function evaluate($submission_num, string $filepath)
     {
-        $submission = Submission::where('id',$submission_num)->first();
-        $testCases = TestCase::where( 'question_id',$submission->question_id)->get();
-        $submissionFile = SubmissionFile::where('submission_id',$submission_num)->first();
+        $submission = Submission::where('id', $submission_num)->first();
+        $testCases = TestCase::where('question_id', $submission->question_id)->get();
+        $submissionFile = SubmissionFile::where('submission_id', $submission_num)->first();
 
         $gradeResult = array(
-            'debug'=>array('line'=>null, 'message'=>null),
-            'log'=>array()
+            'debug' => array('line' => null, 'message' => null),
+            'log' => array()
         );
 
-        foreach($testCases as $testCase)
-        {
-            $command = "python3 ".$filepath.$submissionFile->uuid.'.py';
+        foreach ($testCases as $testCase) {
+            $command = "python3 " . $filepath . $submissionFile->uuid . '.py';
             $process = new Process($command);
             $process->setInput($testCase->input);
             $process->setTimeout(5);
@@ -104,10 +104,10 @@ class EvalFileController extends Controller
                             $numStr .= $row[$iterPos];
                             $iterPos++;
                         }
-
                         $debugInfo = json_decode(Storage::disk('local')->get("file/" . $submissionFile->uuid . '.debug'), true);
                         $gradeResult['debug']['line'] = $debugInfo[intval($numStr)];
                         $gradeResult['debug']['message'] = $this->makeErrorMessage($error);;
+
 
                     }
 
@@ -115,16 +115,16 @@ class EvalFileController extends Controller
                 }
 
             } else if (trim($process->getOutput()) != trim($testCaseOutput)) {
-                    $submission->isCorrect = 0;
-                    $submission->save();
+                $submission->isCorrect = 0;
+                $submission->save();
 
-                    array_push($gradeResult['log'], '실행은 되었으나 잘못된 값이 나왔습니다.');
-                    array_push($gradeResult['log'], "올바른 값:\n" . $testCase->output);
-                    array_push($gradeResult['log'], "프로그램 실행 값:\n" . $process->getOutput());
-                } else {
-                    array_push($gradeResult['log'], '정답입니다!');
-                }
+                array_push($gradeResult['log'], '실행은 되었으나 잘못된 값이 나왔습니다.');
+                array_push($gradeResult['log'], "올바른 값:\n" . $testCase->output);
+                array_push($gradeResult['log'], "프로그램 실행 값:\n" . $process->getOutput());
+            } else {
+                array_push($gradeResult['log'], '정답입니다!');
             }
+        }
         return $gradeResult;
     }
 
@@ -139,26 +139,17 @@ class EvalFileController extends Controller
         preg_match("/TypeError/", $error, $errorTypeErr);
         preg_match("/AttributeError/", $error, $errorTypeAttrErr);
 
-        if($errorTypeName)
-        {
+        if ($errorTypeName) {
             preg_match("/(?)name \'(.*)\'/", $error, $errorName);
-            $errorMsg = $errorMsg.$errorName[0].'변수 선언이 제대로 됐는지 확인하세요.';
-        }
-        else if($errorTypeZeroDiv)
-        {
-            $errorMsg = $errorMsg.'0으로 수를 나눌 수 없습니다.';
-        }
-        else if($errorTypeSyntax)
-        {
-            $errorMsg = $errorMsg.'문법을 확인하세요.';
-        }
-        else if($errorTypeErr)
-        {
-            $errorMsg = $errorMsg.'숫자와 문자를 혼용하여 썼네요. 자료형을 확인해주세요.';
-        }
-        else if($errorTypeAttrErr)
-        {
-            $errorMsg = $errorMsg.'배열 속성이름, 모듈 이름이 잘못되었습니다.';
+            $errorMsg = $errorMsg . $errorName[0] . '변수 선언이 제대로 됐는지 확인하세요.';
+        } else if ($errorTypeZeroDiv) {
+            $errorMsg = $errorMsg . '0으로 수를 나눌 수 없습니다.';
+        } else if ($errorTypeSyntax) {
+            $errorMsg = $errorMsg . '문법을 확인하세요.';
+        } else if ($errorTypeErr) {
+            $errorMsg = $errorMsg . '숫자와 문자를 혼용하여 썼네요. 자료형을 확인해주세요.';
+        } else if ($errorTypeAttrErr) {
+            $errorMsg = $errorMsg . '배열 속성이름, 모듈 이름이 잘못되었습니다.';
         }
 
         return $errorMsg;
